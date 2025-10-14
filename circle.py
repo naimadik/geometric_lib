f63795531c8c839cb8256afa571c9e08586ed5dc
Демьян <demiansof2007@gmail.com>
2025-10-14 08:43:39 +0300

    The documentation in the Readme has been updated, and each of the functions in the .py files has been described.

--- circle.py
+++ circle.py
@@ -2,9 +2,15 @@ import math
 
 
 def area(r):
+    '''Возвращает площадь круга
+    Пример вызова:
+    area(5) вернет 25пи'''
+    
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Возвращает длину окружности 
+    Пример вызова:
+    perimetr(5) вернет 10пи'''
     return 2 * math.pi * r
-
